865057cb1d20892c606f5ae0678797bf059c28b6
edwinnyaribo966-code <edwinnyaribo966@gmail.com>
2026-06-04 13:15:11 +0300

    save to live market

--- deriv.js
+++ deriv.js
@@ -1,6 +1,6 @@
 // Your Configuration
 const APP_ID = '1089';
-const DERIV_WS_URL = `wss://derivws.com?app_id=${APP_ID}`;
+const DERIV_WS_URL = `wss://://derivws.com{APP_ID}`;
 let socket;
 let heartbeatInterval;
 
